6bb090d76a7a83112f46372d907fd87eebda624d
Harrison-Taylor <harrison8420@hotmail.com>
2025-02-08 14:42:13 -0600

    working version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 BookBot is my first project!
+Now that I have finished the bookbot and have had a chance to look at the submitted solutions on book.dev, I realize my method for creating the dictionay of number of times each character appearing in the book was very inffecient. It has to read through the whole book 26 times to make the list. The provided solution of just reading through the book once and adding each new character to a dictionary is much better. I also didn't make nearly enough of my code generalized for easy use later (not saving book path as a variable etc..)
 # bookbot
--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     length = words(file_contents)
-    number_of_letters = characters(file_contents)    
-    return print(length), print(number_of_letters)
+    number_of_letters = characters(file_contents) 
+    report = char_count_report(number_of_letters)
+    
+    print("Report of Frankenstein")
+    print(f"{length} words found in the book")
+    print(report)
+    for letter in report:
+        print(f"The '{letter["char"]}' was found {letter["num"]} times")
 
 def words(text):
     split = text.split()
@@ -14,7 +20,7 @@ def characters(text):
     char_dictionary = {}
     lowered_string = text.lower()
     lowered_letters = list(lowered_string)
-    alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     for letter in alphabet:
         count = 0
         for character in lowered_letters:
@@ -22,5 +28,15 @@ def characters(text):
                 count += 1
         char_dictionary[letter] = count
     return char_dictionary
+
+def sort_on(dict):
+    return dict["num"]
+
+def char_count_report(character_dictionary):
+    sorted_characters = []
+    for character in character_dictionary:
+        sorted_characters.append({"char": character, "num": character_dictionary[character]})
+    sorted_characters.sort(reverse=True, key=sort_on)
+    return sorted_characters
 
 main()
